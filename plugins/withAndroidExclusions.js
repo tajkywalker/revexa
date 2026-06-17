3c6ecc31257c7adcd0fe7b04d3053d524ad38027
Kilo Fix <fix@revexa.cz>
2026-06-17 20:09:44 +0000

    fix: apply exclusions to both app/build.gradle and root build.gradle

--- plugins/withAndroidExclusions.js
+++ plugins/withAndroidExclusions.js
@@ -1,26 +1,57 @@
-const { withProjectBuildGradle } = require("@expo/config-plugins");
+const { withAppBuildGradle, withProjectBuildGradle } = require("@expo/config-plugins");
 
 const MARKER = "// Fix: exclude old com.android.support to avoid duplicate classes";
 
-const exclusionBlock = `
+const appExclusionBlock = `
 ${MARKER}
-subprojects {
-  configurations.all {
+configurations.all {
     exclude group: "com.android.support", module: "support-compat"
     exclude group: "com.android.support", module: "support-media-compat"
     exclude group: "com.android.support", module: "support-core-utils"
     exclude group: "com.android.support", module: "support-annotations"
     exclude group: "com.android.support", module: "support-fragment"
     exclude group: "com.android.support", module: "appcompat-v7"
-  }
+    exclude group: "com.android.support", module: "support-v4"
+    exclude group: "com.android.support", module: "support-core-ui"
+    exclude group: "com.android.support", module: "animated-vector-drawable"
+    exclude group: "com.android.support", module: "support-vector-drawable"
+}
+`;
+
+const projectExclusionBlock = `
+${MARKER}
+subprojects {
+    configurations.all {
+        exclude group: "com.android.support", module: "support-compat"
+        exclude group: "com.android.support", module: "support-media-compat"
+        exclude group: "com.android.support", module: "support-core-utils"
+        exclude group: "com.android.support", module: "support-annotations"
+        exclude group: "com.android.support", module: "support-fragment"
+        exclude group: "com.android.support", module: "appcompat-v7"
+        exclude group: "com.android.support", module: "support-v4"
+        exclude group: "com.android.support", module: "support-core-ui"
+        exclude group: "com.android.support", module: "animated-vector-drawable"
+        exclude group: "com.android.support", module: "support-vector-drawable"
+    }
 }
 `;
 
 module.exports = function withAndroidExclusions(config) {
-  return withProjectBuildGradle(config, (config) => {
+  // Apply to android/app/build.gradle (direct fix for :app:checkReleaseDuplicateClasses)
+  config = withAppBuildGradle(config, (config) => {
     if (!config.modResults.contents.includes(MARKER)) {
-      config.modResults.contents += exclusionBlock;
+      config.modResults.contents += appExclusionBlock;
     }
     return config;
   });
+
+  // Apply to android/build.gradle as well (subprojects broadcover)
+  config = withProjectBuildGradle(config, (config) => {
+    if (!config.modResults.contents.includes(MARKER)) {
+      config.modResults.contents += projectExclusionBlock;
+    }
+    return config;
+  });
+
+  return config;
 };
